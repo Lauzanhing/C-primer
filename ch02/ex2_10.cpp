54ef3bc1900ae8be420da644f0a8f6a8e9971810
lauzanhing <lauzanhing@gmail.com>
2018-02-28 19:39:03 +0800

    update

--- ch02/ex2_10.cpp
+++ ch02/ex2_10.cpp
@@ -9,6 +9,6 @@ int main()
 global_str is global variable, so the value is empty string. global_int is global variable,
 so the value is zero. local_int is a local variable which is uninitialized, so it has a undefined value.
 local_str is also a local variable which is uninitialized, but it has a value that is defined by the class.
-So it is empty string. PS: please read P44 in the English version, P40 in Chinese version to get more. 
-The note: Uninitialized objects of built-in type defined inside a function body have a undefined value. 
+So it is empty string. PS: please read P44 in the English version, P40 in Chinese version to get more.
+The note: Uninitialized objects of built-in type defined inside a function body have a undefined value.
 Objects of class type that we do not explicitly inititalize have a value that is defined by class.
